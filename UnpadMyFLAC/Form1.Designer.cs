563b49374cc28580dbc86a8c3d2cb30b6318e0dd
Pedro Lourenço <madmilkpt@gmail.com>
2018-05-21 13:07:52 +0100

    Initial commit

---
+++ UnpadMyFLAC/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UnpadMyFLAC
+{
+	partial class FormMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonOpen = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(72, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Choose folder";
+			// 
+			// buttonOpen
+			// 
+			this.buttonOpen.Location = new System.Drawing.Point(90, 12);
+			this.buttonOpen.Name = "buttonOpen";
+			this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+			this.buttonOpen.TabIndex = 2;
+			this.buttonOpen.Text = "Open Folder";
+			this.buttonOpen.UseVisualStyleBackColor = true;
+			this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(187, 43);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonOpen);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "FormMain";
+			this.Text = "UnpadMyFLAC";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button buttonOpen;
+	}
+}
+
---
+++ UnpadMyFLAC/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnpadMyFLAC
+{
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
+		/*String	directory:		full path to the file, not including the filename			Eg.		"C:\foo\bar.txt"
+					fileEntries:	full path to a list of files, including the filenames		Eg.	[0]	"C:\latin\lorem.txt"
+																									[1]	"C:\latin\ipsum.txt"
+					file:			a single position of the array "fileEntries"				Eg.	[1]	"C:\latin\ipsum.txt"
+					fileName:		the filename of "file", including extension					Eg.		"ipsum.txt"*/
+
+
+		private void buttonOpen_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog
+			{
+				Filter = "Any file|*.*",
+				Title = "Select a file from a folder you want to read dates and write names"
+			};
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				String directory = Path.GetDirectoryName(openFileDialog.FileName);
+				String[] fileEntries = Directory.GetFiles(directory);
+				//UInt64 i = 1;                           //Counter
+				foreach (String file in fileEntries)
+				{
+					//String fileName = Path.GetFileName(file);
+
+					Process process = new Process();
+					process.StartInfo.UseShellExecute = true;
+					process.StartInfo.RedirectStandardOutput = false;
+					process.StartInfo.RedirectStandardOutput = false;
+					process.StartInfo.FileName = "metaflac.exe";
+					process.StartInfo.Arguments = String.Concat("--remove --block-type=PADDING --dont-use-padding \"", file, '\"');
+
+					process.Start();
+				}
+			}
+		}
+	}
+}
